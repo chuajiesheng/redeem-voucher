baf5a794d0777d9482b19bc65f15108a9e8bd0ec
Chua Jie Sheng <chuajiesheng@gmail.com>
2018-06-23 00:30:49 +0800

    Check if the voucher exist, if exist then send the download link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,14 @@ class HomeController < ApplicationController
   end
 
   def redeem
+    voucher_uuid = params.permit(:voucher)[:voucher]
+    voucher = Voucher.find_by_uuid(voucher_uuid)
+
+    if not voucher
+      flash[:error] = 'Please provide a valid voucher code.'
+      return redirect_to root_path
+    end
+
     cheatsheet_path = File.join(Rails.root, 'app','assets', 'pdfs', 'cheatsheet.pdf')
     send_file cheatsheet_path
   end
